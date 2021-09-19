0d071b0c689528b4055163ffb9723c136ff2fac4
gabrillos2099 <55769161+gabrillos2099@users.noreply.github.com>
2021-09-19 20:21:46 -0300

    Add files via upload

---
+++ FizzBuzz.js
@@ -0,0 +1,17 @@
+let FizzBuzz = (numA, numB, p1, p2, p3) => {
+    for (let contagem=0; contagem<=100; contagem++){
+        if (contagem % numA == 0 && contagem % numB ==0 ){
+            console.log (p1)
+
+        }else if (contagem % numA == 0){
+            console.log(p2);
+        }else if(contagem % numB == 0){
+            console.log(p3);
+        }else{
+            console.log(contagem);
+        }
+        
+    }
+    
+}
+console.log(FizzBuzz(15,2, 'FizzBuzz', 'Fizz','Buzz'));
